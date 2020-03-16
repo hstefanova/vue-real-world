3c2472945a52b20471bb441c8ad59ee2a76e6552
hstefanova <hstefanova@github.com>
2020-03-16 18:50:46 +0200

    Add Axios to the project

--- real-world-project/src/components/EventCard.vue
+++ real-world-project/src/components/EventCard.vue
@@ -1,28 +1,22 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: event.id } }"
+  >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <p>Location: {{ event.location }}</p>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tue May 15 2018',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Adam Jahr' },
-          { id: 'def456', name: 'Gregg Pollack' }
-        ]
-      }
+  props: {
+    event: {
+      type: Object
     }
   }
 }
--- real-world-project/src/views/EventList.vue
+++ real-world-project/src/views/EventList.vue
@@ -3,17 +3,31 @@
     <div>This is Event List View</div>
 
     <h4>Events:</h4>
-    <EventCard />
-    <EventCard />
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
 export default {
+  data() {
+    return {
+      events: []
+    }
+  },
   components: {
     EventCard
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error =>
+        console.log('There is an error with loading the file.' + error.response)
+      )
   }
 }
 </script>
