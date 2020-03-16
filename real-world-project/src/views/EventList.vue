bb9fb603c715482d107b0a5399f07a655a9af25f
hstefanova <hstefanova@github.com>
2020-03-16 11:58:26 +0200

    add router functionality

---
+++ real-world-project/src/views/EventList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>This is Event List View</div>
+
+    <h4>Events:</h4>
+    <EventCard />
+    <EventCard />
+    <EventCard />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
+
+<style></style>
---
+++ real-world-project/src/views/EventShow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Showing event #{{ id }}</h1>
+
+    <h2>Event title</h2>
+    <span>Event Subtitle</span>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+      ratione minus debitis harum laborum similique alias deserunt aperiam nisi
+      cupiditate?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style></style>
