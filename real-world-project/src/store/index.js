f229cb95a7e5a86b85a9f10d32439b46dedcc47d
hstefanova <hstefanova@github.com>
2020-03-17 17:00:19 +0200

    add state and getters

--- real-world-project/src/store/index.js
+++ real-world-project/src/store/index.js
@@ -1,11 +1,34 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Adam Johnson'
+    },
+    categories: ['sustainability', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done).length
+    },
+    activeTodos: state => {
+      return state.todos.filter(todo => !todo.done).length
+    }
+  }
+})
--- real-world-project/src/views/EventCreate.vue
+++ real-world-project/src/views/EventCreate.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <h1>Create Event</h1>
+    <h1>Create Event, {{ user.name }} with id: {{ user.id }}</h1>
+
+    <p>There are {{ catLengh }} categories here:</p>
+    <p>There are still {{ activeTodos }} to be done.</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+export default {
+  //access state properties
+  computed: {
+    catLengh() {
+      return this.$store.getters.catLength
+    },
+    activeTodos() {
+      return this.$store.getters.activeTodos
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
 </script>
 
 <style></style>
